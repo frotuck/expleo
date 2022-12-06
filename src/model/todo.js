1b121b0523aa93c577a3af96a8ec87b39600c0bb
Jenseneducation-Lukas-Tucker <54941820+Jenseneducation-Lukas-Tucker@users.noreply.github.com>
2022-12-06 14:13:06 +0100

    added diagram

--- src/model/todo.js
+++ src/model/todo.js
@@ -1,20 +1,22 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const TodoSchema = new Schema({
-    title: String,
-    status: String,
-    due_date: Number, // epoch time
-    create_date: Number, //epoch time
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
-    href: String
+	title: String,
+	status: String,
+	due_date: Number, // epoch time
+	create_date: Number, //epoch time
+	createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+	href: String,
 });
 
 const userSchema = new Schema({
-    name: String,
-    todos: [{ type: Schema.Types.ObjectId, ref: 'Todo' }],
-    href: String
+	name: String,
+	todos: [{ type: Schema.Types.ObjectId, ref: 'Todo' }],
+	href: String,
 });
 
-
-module.exports ={Todo: mongoose.model('Todo', TodoSchema), User: mongoose.model('User', userSchema)};
+module.exports = {
+	Todo: mongoose.model('Todo', TodoSchema),
+	User: mongoose.model('User', userSchema),
+};
